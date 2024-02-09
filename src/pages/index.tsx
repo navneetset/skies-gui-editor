8a52022ef7b135ad3e3e612fb3112bac63e27046
navneetset <navneetset@gmail.com>
2024-02-09 01:45:48 +0100

    Title change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -262,7 +262,7 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>SkiesGUI Editor</title>;
 
 const PageStyles = styled.div`
   display: flex;
